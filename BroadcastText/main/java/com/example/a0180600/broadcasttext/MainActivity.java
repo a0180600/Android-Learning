23ef6fefedbd89f486de5c806a3fcc0bf9e6d211
a0180600 <505128489@qq.com>
2016-08-11 16:25:18 +0800

    BroadcastText v3.0

--- BroadcastText/main/java/com/example/a0180600/broadcasttext/MainActivity.java
+++ BroadcastText/main/java/com/example/a0180600/broadcasttext/MainActivity.java
@@ -8,6 +8,8 @@ import android.content.IntentFilter;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 /**
@@ -23,10 +25,15 @@ public class MainActivity extends Activity {
         super.onCreate(savadInstanceState);
         setContentView(R.layout.activity_main);
 
-        intentFilter = new IntentFilter();
-        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
-        networkChangeReceiver = new NetworkChangeReceiver();
-        registerReceiver(networkChangeReceiver, intentFilter);
+        Button button = (Button) findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent("com.example.a0180600.broadcasttext.MY_BROADCAST");
+                sendBroadcast(intent);
+            }
+        });
+
     }
 
     @Override
---
+++ BroadcastText/main/java/com/example/a0180600/broadcasttext/MyBroadcastReceiver.java
@@ -0,0 +1,18 @@
+package com.example.a0180600.broadcasttext;
+
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.Toast;
+
+/**
+ * Created by a0180600 on 16-8-11.
+ */
+public class MyBroadcastReceiver extends BroadcastReceiver {
+
+    @Override
+    public void onReceive(Context context, Intent intent) {
+        Toast.makeText(context, "received in MyBroadcastReceiver", Toast.LENGTH_SHORT).show();
+    }
+
+}

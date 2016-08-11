48fec947ecd7c10dd7cdb8a40b9ae3278cfe7af4
a0180600 <505128489@qq.com>
2016-08-11 15:12:30 +0800

    BroadcastText v1.0

---
+++ BroadcastText/main/java/com/example/a0180600/broadcasttext/BootCompleteReceiver.java
@@ -0,0 +1,11 @@
+package com.example.a0180600.broadcasttext;
+
+import android.content.BroadcastReceiver;
+
+/**
+ * Created by a0180600 on 16-8-11.
+ */
+public class BootCompleteReceiver extends BroadcastReceiver {
+
+    
+}
---
+++ BroadcastText/main/java/com/example/a0180600/broadcasttext/MainActivity.java
@@ -0,0 +1,54 @@
+package com.example.a0180600.broadcasttext;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
+import android.os.Bundle;
+import android.widget.Toast;
+
+/**
+ * Created by a0180600 on 16-8-11.
+ */
+public class MainActivity extends Activity {
+
+    private IntentFilter intentFilter;
+    private NetworkChangeReceiver networkChangeReceiver;
+
+    @Override
+    protected void onCreate(Bundle savadInstanceState) {
+        super.onCreate(savadInstanceState);
+        setContentView(R.layout.activity_main);
+
+        intentFilter = new IntentFilter();
+        intentFilter.addAction("android.net.conn.CONNECTIVITY_CHANGE");
+        networkChangeReceiver = new NetworkChangeReceiver();
+        registerReceiver(networkChangeReceiver, intentFilter);
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(networkChangeReceiver);
+    }
+
+    class NetworkChangeReceiver extends BroadcastReceiver {
+
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            ConnectivityManager connectivityManager = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+            NetworkInfo networkInfo = connectivityManager.getActiveNetworkInfo();
+            if (networkInfo != null && networkInfo.isAvailable()) {
+                Toast.makeText(context, "network is available", Toast.LENGTH_SHORT);
+            } else {
+
+                Toast.makeText(context, "network is unavailable", Toast.LENGTH_SHORT);
+            }
+        }
+
+    }
+
+}

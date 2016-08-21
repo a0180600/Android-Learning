9abf0a00e04dbe9b1b30a60d80ae0ff23f2eec8b
a0180600 <505128489@qq.com>
2016-08-21 17:34:06 +0800

    ServiceTest v2.0

--- ServiceTest/main/java/com/example/a0180600/servicetest/MainActivity.java
+++ ServiceTest/main/java/com/example/a0180600/servicetest/MainActivity.java
@@ -1,10 +1,44 @@
 package com.example.a0180600.servicetest;
 
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 /**
  * Created by a0180600 on 16-8-21.
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener {
+
+    private Button startService;
+    private Button stopService;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        startService = (Button) findViewById(R.id.start_service);
+        stopService = (Button) findViewById(R.id.stop_service);
+        startService.setOnClickListener(this);
+        stopService.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.start_service:
+                Intent startIntent = new Intent(this, MyService.class);
+                startService(startIntent);
+                break;
+            case R.id.stop_service:
+                Intent stopIntent = new Intent(this, MyService.class);
+                stopService(stopIntent);
+                break;
+            default:
+                break;
+        }
+    }
 
 }
--- ServiceTest/main/java/com/example/a0180600/servicetest/MyService.java
+++ ServiceTest/main/java/com/example/a0180600/servicetest/MyService.java
@@ -3,6 +3,7 @@ package com.example.a0180600.servicetest;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 /**
  * Created by a0180600 on 16-8-21.
@@ -17,16 +18,19 @@ public class MyService extends Service {
     @Override
     public void onCreate() {
         super.onCreate();
+        Log.d("MyService", "onCreate executed");
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d("MyService", "onStartCommand executed");
         return super.onStartCommand(intent, flags, startId);
     }
 
     @Override
     public void onDestroy() {
         super.onDestroy();
+        Log.d("MyService", "onDestory executed");
     }
 
 }

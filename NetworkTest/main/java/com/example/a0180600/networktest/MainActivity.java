53261559c7e421a4f32dc1e3152b65725cc7528f
a0180600 <505128489@qq.com>
2016-08-22 16:12:22 +0800

    NetworkTest v2.0

--- NetworkTest/main/java/com/example/a0180600/networktest/MainActivity.java
+++ NetworkTest/main/java/com/example/a0180600/networktest/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.a0180600.networktest;
 
 import android.app.Activity;
+import android.content.Entity;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
@@ -46,8 +47,34 @@ public class MainActivity extends Activity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
         if (v.getId() == R.id.send_request) {
-            sendRequestWithHttpURLConnection();
+            // sendRequestWithHttpURLConnection();
+            sendRequestWithHttpClient();
         }
+    }
+
+    private void sendRequestWithHttpClient() {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    HttpClient httpClient = new DefaultHttpClient();
+                    HttpGet httpGet = new HttpGet("http://www.baidu.com");
+                    HttpResponse httpResponse = httpClient.execute(httpGet);
+                    if (httpResponse.getStatusLine().getStatusCode() == 200) {
+                        // 请求和响应都成功了
+                        HttpEntity entity = httpResponse.getEntity();
+                        String response = EntityUtils.toString(entity, "utf-8");
+                        Message message = new Message();
+                        message.what = SHOW_RESPONSE;
+                        // 将服务器返回的结果存放在Message
+                        message.obj = response.toString();
+                        handler.sendMessage(message);
+                    }
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }).start();
     }
 
     private void sendRequestWithHttpURLConnection() {
